6d7d7379056061b7ef18b34685ac76cad4e51b61
kc <kevincabugao3@gmail.com>
2020-09-21 01:36:21 +0800

    initial

---
+++ src/main/java/com/acn/demo/webinar/WebinarApplication.java
@@ -0,0 +1,13 @@
+package com.acn.demo.webinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebinarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebinarApplication.class, args);
+    }
+
+}
